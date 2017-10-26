184f21867cf14e46a63eef8ab0b1fc8afb09d134
Andrew Guy <andrewguy@burnet.edu.au>
2017-10-26 11:55:45 +1100

    Update version

--- biostructmap/__init__.py
+++ biostructmap/__init__.py
@@ -7,5 +7,5 @@ such as Tajima's D.
 
 from .biostructmap import Structure, SequenceAlignment
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 __all__ = ["biostructmap", "seqtools", "pdbtools", "gentests", "map_functions"]
